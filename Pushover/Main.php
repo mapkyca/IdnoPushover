28ce0c4a45ab4c047c0e79a1663f56775b31f0bf
Marcus Povey <marcus@marcus-povey.co.uk>
2013-09-22 17:09:08 +0100

    Should now be functional.

--- Pushover/Main.php
+++ Pushover/Main.php
@@ -12,10 +12,54 @@ namespace IdnoPlugins\Pushover {
             // Register the callback URL
             \Idno\Core\site()->addPageHandler('account/pushover', '\IdnoPlugins\Pushover\Pages\Account');
         }
+        
+        function registerEventHooks() {
+            parent::registerEventHooks();
+            
+            \Idno\Core\site()->addEventHook('annotation/add/reply', 'self::annotate');
+        }
+        
+        static function annotate(\Idno\Core\Event $event) {
+            $object = $event->data()['object'];
+            $annotation = $this->data()['annotation'];
+            if ($user = $object->getOwner() )
+            {
+                $session = \Idno\Core\site()->session(); 
+                $logged_in_user = $session->currentUser(); 
+                
+                if ($user->getUUID() != $logged_in_user->getUUID()) {
+                    
+                    $user_token = $user->pushover_user_token;
+                    $token = $user->pushover_app_token;
+                    $title = $object->getTitle();
+                    $url = $object->getUrl();
+                    
+                    if (!empty($user_token) && !empty($token)) {
 
-        static function send($token, $user_token, $message) {
+                        // Not replying to my own stuff, so we can send a pushover message
+                        switch ($event->getName()) {
+
+                            case 'annotation/add/reply' :
+                                    self::send($token, $user_token, "You have a new comment", ['title' => $title, 'url' => $url]);
+                                break;
+                        }
+                        
+                    }
+                }
+                
+                
+            }
+            
+        }
+
+        static function send($token, $user_token, $message, array $params = null) {
             
             $req = 'token=' . urlencode($token) . '&user=' . urlencode($user_token) . '&message=' . urlencode($message);
+            if ($params) {
+                foreach ($params as $key => $value)
+                    $req .= "&$key=" . urlencode($value);
+            }
+            
 
             $curl_handle = curl_init();
             curl_setopt($curl_handle, CURLOPT_URL, 'https://api.pushover.net/1/messages.json');
